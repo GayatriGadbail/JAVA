22e98a88cf5123a560998ea5b0ec1325ee0fa337
GayatriGadbail <gdgadbail2003@gmail.com>
2025-01-30 12:02:37 +0530

    Update Demo.java

--- GitDemo/src/com/Demo.java
+++ GitDemo/src/com/Demo.java
@@ -1,6 +1,10 @@
 package com;
 
 public class Demo {
+	public void m1()
+	{
+		System.out.println("m1--A");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("This is main method");
